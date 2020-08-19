8a95ceae6ae297d63a0f7f223a8832bb5136665a
Helen Xu <helenxuzh@gmail.com>
2020-08-18 17:43:51 -0700

    Updated nav bar

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav-bar").style.top = "0";
+    // document.getElementById("nav-bar").style.backgroundColor = "hsla(0, 0%, 20%, 0.85)";
+  } else {
+    document.getElementById("nav-bar").style.top = "-96px";
+  }
+  prevScrollpos = currentScrollPos;
+}
